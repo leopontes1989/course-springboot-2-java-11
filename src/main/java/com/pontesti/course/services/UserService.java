ad4c1d97d936efc9bf44d31971cb4d9230370c8b
leopontes1989 <leopontes.1989@gmail.com>
2021-06-02 12:24:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/pontesti/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pontesti.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pontesti.course.entities.User;
+import com.pontesti.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById (Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
